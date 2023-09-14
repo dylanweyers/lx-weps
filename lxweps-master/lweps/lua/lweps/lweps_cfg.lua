2aafcd7aad46c1aec92b77c4b5be0950530bb07a
Dylan Weyers <59836383+dylanweyers@users.noreply.github.com>
2023-09-14 13:47:25 +0100

    Add files via upload

---
+++ lxweps-master/lweps/lua/lweps/lweps_cfg.lua
@@ -0,0 +1,20 @@
+// Config file
+lwep = {}
+
+// Strings to be recognized as commands that will open the menu
+lwep.cmds = {
+	["!permweps"] = true,
+	["!lweps"] = true,
+}
+
+// Table of specific users that can access the menu regardless of their in-game rank
+lwep.users = {
+	["STEAM_X:XXXXXXXX"] = true,
+}
+
+// Table of in-game ranks that can open the menu
+lwep.ranks = {
+	["owner"] = true,
+	["co-owner"] = true,
+	["superadmin"] = true,
+}
